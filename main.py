8dd71fbd3f8ae75aa0572978150847891733eee1
dapanan <qc7qjfs5xm@privaterelay.appleid.com>
2026-02-01 19:12:04 +0300

    тест

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 from application import GameWindow
-from logic.assets.asset_manager import AssetManager
-from logic.assets.sound_manager import SoundManager
-
 
 def main():
-    asset_manager = AssetManager()
-    asset_manager.load_all()
-
-    sound_manager = SoundManager()
-    sound_manager.load_all()
-
-    window = GameWindow(asset_manager, sound_manager)
+    window = GameWindow()
     window.run()
-
 
 if __name__ == "__main__":
     main()
